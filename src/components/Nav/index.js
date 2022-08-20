e77f0d1b1afc9188e98be6bfcbc985bd37daa9bb
drkevinfriday <kevinfriday87@gmail,com>
2022-08-20 14:58:38 -0500

    test

---
+++ src/components/Nav/index.js
@@ -0,0 +1,79 @@
+import React from "react";
+import {capitalizeFirstLetter } from "../../utils/helpers";
+
+
+function Nav (props){
+
+    // const handleClick = () => {
+    //     console.log("click handled")
+        
+    //   }
+
+    const {
+        categories = [],
+        currentCategory,
+        setCurrentCategory,
+        setContactSelected,
+        contactSelected
+    }= props;
+
+    // useEffect(()=> {
+    //     document.title = capitalizeFirstLetter(currentCategory.name);
+    // },[currentCategory]);
+    
+    // const handleClick = (item) => {
+    //     console.log(item);
+    //     return item;
+    //   };
+
+    
+
+    return(
+        <header>
+            <h2>       
+                <a data-testid= "link" href ="/">
+                    <span role="img" aria-label="camera">📸</span> Kevin Friday Portfolio
+                </a>   
+            </h2>     
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        <a 
+                        data-testid ='about'
+                         href="#about" onClick={()=> setContactSelected(false)}
+                         >
+                            About me 
+                        </a>
+                    </li>
+                    <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+                        <span onClick={()=> setContactSelected(true)}>
+                            Contact
+                        </span>
+                    </li>
+                
+                     {
+                        categories.map((category) => (
+                        <li className={`mx-1 ${
+                            currentCategory.name === category.name && !contactSelected && 'navActive'
+                        }`}key={category.name}>
+
+                            <span 
+                            onClick={()=> {
+                                setCurrentCategory(category)
+                                setContactSelected(false)
+                            }}
+                            >
+                            {capitalizeFirstLetter(category.name)}
+
+                            </span>
+                        </li>
+                        ))
+                    }
+                    
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Nav
