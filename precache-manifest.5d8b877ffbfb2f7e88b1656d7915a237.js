70daa2c32aefa3fb22c44537f2451d9f387ec30e
drkevinfriday <kevinfriday87@gmail,com>
2022-08-31 23:00:59 -0500

    Updates

---
+++ precache-manifest.5d8b877ffbfb2f7e88b1656d7915a237.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b2b333a6f4f00056690",
+    "url": "/Portfolio_React/static/css/main.6e816bd3.chunk.css"
+  },
+  {
+    "revision": "4b2b333a6f4f00056690",
+    "url": "/Portfolio_React/static/js/main.4b2b333a.chunk.js"
+  },
+  {
+    "revision": "0d71e04f289fc819143c",
+    "url": "/Portfolio_React/static/js/1.0d71e04f.chunk.js"
+  },
+  {
+    "revision": "fe681ae76874c2a20440",
+    "url": "/Portfolio_React/static/js/2.fe681ae7.chunk.js"
+  },
+  {
+    "revision": "798bc13f0703407563a3",
+    "url": "/Portfolio_React/static/js/runtime~main.798bc13f.js"
+  },
+  {
+    "revision": "8380c1fac0c0886a550f3985a5080741",
+    "url": "/Portfolio_React/static/media/ProfilePic.8380c1fa.jpg"
+  },
+  {
+    "revision": "30884c4190b1689b4fe9b84f52e11a57",
+    "url": "/Portfolio_React/static/media/hodl.30884c41.png"
+  },
+  {
+    "revision": "fc0046d0e73fa2f5cd938c3f3ee10b5c",
+    "url": "/Portfolio_React/static/media/hodl2.fc0046d0.PNG"
+  },
+  {
+    "revision": "2cfd340d4a322c5372cbbfd3e39f27bf",
+    "url": "/Portfolio_React/static/media/hodl3.2cfd340d.PNG"
+  },
+  {
+    "revision": "0e51a581a926f4cd6b97c7d0d61bc493",
+    "url": "/Portfolio_React/static/media/hodl4.0e51a581.PNG"
+  },
+  {
+    "revision": "1e155d0a9b5fcfbe9f93ac1910858fbf",
+    "url": "/Portfolio_React/static/media/quiz.1e155d0a.PNG"
+  },
+  {
+    "revision": "538fd0899bd5f13e74bc33d22d87ef11",
+    "url": "/Portfolio_React/static/media/quiz2.538fd089.PNG"
+  },
+  {
+    "revision": "221aa1c14e6dbc2dffc2edf76398f7f1",
+    "url": "/Portfolio_React/static/media/quiz3.221aa1c1.PNG"
+  },
+  {
+    "revision": "0f5ccbe51674894d474f2ef2a0225417",
+    "url": "/Portfolio_React/static/media/quiz4.0f5ccbe5.PNG"
+  },
+  {
+    "revision": "ae1023fbb6c9d329533dd2af8afb1668",
+    "url": "/Portfolio_React/static/media/weatherapi.ae1023fb.PNG"
+  },
+  {
+    "revision": "fe1404d13dc2ca01f136cf9b6ec2937d",
+    "url": "/Portfolio_React/static/media/weatherapi2.fe1404d1.PNG"
+  },
+  {
+    "revision": "8b5bebf6bc87e3da64ef27cb6ade2b73",
+    "url": "/Portfolio_React/static/media/weatherapi3.8b5bebf6.PNG"
+  },
+  {
+    "revision": "c8eca124a859a47ea1022960b0861b09",
+    "url": "/Portfolio_React/static/media/weatherapi4.c8eca124.PNG"
+  },
+  {
+    "revision": "63e471ba0868c6ef0e1dc889417cdf90",
+    "url": "/Portfolio_React/static/media/notetaker.63e471ba.PNG"
+  },
+  {
+    "revision": "ccf462b98a9113e7a070296623e5288c",
+    "url": "/Portfolio_React/static/media/notetaker1.ccf462b9.PNG"
+  },
+  {
+    "revision": "3e210e9053c14bbfa082492e98f1430c",
+    "url": "/Portfolio_React/static/media/notetaker2.3e210e90.PNG"
+  },
+  {
+    "revision": "cbbb7b417429af45c7a1f482844127ce",
+    "url": "/Portfolio_React/static/media/notetaker3.cbbb7b41.PNG"
+  },
+  {
+    "revision": "b3c4afb2a2187393881d0b84f80a0058",
+    "url": "/Portfolio_React/static/media/EmployeeAide.b3c4afb2.PNG"
+  },
+  {
+    "revision": "325a4b9badcc26394b2be8e7726c7ebc",
+    "url": "/Portfolio_React/static/media/EmployeeAide2.325a4b9b.PNG"
+  },
+  {
+    "revision": "f958dc1ddde81a00d63bc7c46c91b300",
+    "url": "/Portfolio_React/static/media/EmployeeAide3.f958dc1d.PNG"
+  },
+  {
+    "revision": "d71975e535808576bdfabb07e1b9cc63",
+    "url": "/Portfolio_React/static/media/EmployeeAide4.d71975e5.PNG"
+  },
+  {
+    "revision": "4fcdb7b77dfe800da4613f73525a5656",
+    "url": "/Portfolio_React/static/media/ism.4fcdb7b7.PNG"
+  },
+  {
+    "revision": "f2d67751863ca52d580ed6a8a3098e89",
+    "url": "/Portfolio_React/static/media/ism2.f2d67751.png"
+  },
+  {
+    "revision": "19b7c2b9ce294924dd7f3e98a5338a4f",
+    "url": "/Portfolio_React/static/media/ism3.19b7c2b9.png"
+  },
+  {
+    "revision": "700ddff2cff8fd88804a90374e7faccb",
+    "url": "/Portfolio_React/static/media/ism4.700ddff2.png"
+  },
+  {
+    "revision": "cc554fb16d84d6ab2a5c0a5fc1004c2c",
+    "url": "/Portfolio_React/e7e915313973ba1d49f9bb2a1998626e.js"
+  },
+  {
+    "revision": "76532baffaf93398305818ba2981b293",
+    "url": "/Portfolio_React/index.html"
+  }
+];
